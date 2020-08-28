5e022912d3910b7dd386def5cbc9433f70ad7c2f
ardagokce <ardagokce96@hotmail.com>
2020-08-28 17:30:37 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Header = () => {
+
+    const headerStyle = {
+        padding: "20px 0",
+        lineHeignt: "2em",
+    }
+
+    return(
+        <header style={headerStyle}>
+            <h1 style={{
+                fontSize: "25px", marginBottom: "15px"}}>
+                    Simple Todo App
+                    </h1>
+            <p style={{
+                fontSize: "19px"}}>
+                    Please add to-dos item(s) through the input field
+                    </p>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// index.js
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+//component file
+import TodoContainer from "./components/TodoContainer"
+import "./App.css"
+
+
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
